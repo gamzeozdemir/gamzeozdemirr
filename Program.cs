0be7ee1c26a6d1d20fbbdf36f5976304476606dc
Gamze <=gamze4249@gmail.com>
2023-11-17 02:18:03 +0300

    odev
    fibonacci sayılarını yazdıran algoritma

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Fibonacci dizisinin kaç terimini görmek istiyorsunuz? ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Fibonacci Dizisi:");
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(Fibonacci(i) + " ");
+        }
+
+        Console.ReadLine();
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+        else
+        {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
